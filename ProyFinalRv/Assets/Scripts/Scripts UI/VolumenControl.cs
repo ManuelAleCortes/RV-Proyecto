ff36b4c2887af867e40ef70af92c9d888515764a
Juan Pablo Hurtado Agudelo <123783172+juanpabloh18@users.noreply.github.com>
2024-05-15 20:25:48 -0500

    función de pausar y reproducir la musica

--- ProyFinalRv/Assets/Scripts/Scripts UI/VolumenControl.cs
+++ ProyFinalRv/Assets/Scripts/Scripts UI/VolumenControl.cs
@@ -5,26 +5,42 @@ using UnityEngine.UI;
 
 public class VolumenControl : MonoBehaviour
 {
-    public Slider volumeSlider; // Referencia al slider de la UI
-    public AudioSource audioSource; // Referencia al componente AudioSource
+    public Slider volumeSlider; 
+    public AudioSource audioSource; 
+    public Button pauseButton; 
+    public Button playButton; 
 
     void Start()
     {
-        // Asegurarse de que el slider esté configurado entre 0 y 1
+       
         volumeSlider.minValue = 0;
         volumeSlider.maxValue = 1;
 
-        // Configurar el valor inicial del slider y el audio source a 0
+        
         volumeSlider.value = 0;
         audioSource.volume = 0;
 
-        // Agregar un listener al slider para que llame a la función OnVolumeChange cuando se cambie el valor
+        
         volumeSlider.onValueChanged.AddListener(OnVolumeChange);
+        pauseButton.onClick.AddListener(PauseAudio);
+        playButton.onClick.AddListener(PlayAudio);
     }
 
     void OnVolumeChange(float value)
     {
-        // Cambiar el volumen del audio source al valor del slider
+        
         audioSource.volume = value;
+    }
+
+    void PauseAudio()
+    {
+       
+        audioSource.Pause();
+    }
+
+    void PlayAudio()
+    {
+        // Reproducir el audio
+        audioSource.Play();
     }
 }
